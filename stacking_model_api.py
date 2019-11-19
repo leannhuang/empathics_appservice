47aecde92a654ddd4f5d82c5c267a9b84c03631b
LeAnn <aya65535@gmail.com>
2019-11-19 16:32:57 +0000

    stacking model

--- stacking_model_api.py
+++ stacking_model_api.py
@@ -2,21 +2,21 @@ import urllib.request
 import json
 
 def send_request_to_ml(row):
-    smile = row[0]
-    gender = row[1]
-    anger = row[2]
-    comtempt = row[3]
-    disgust = row[4]
-    fear = row[5]
-    happiness = row[6]
-    neutral = row[7]
-    sadness = row[8]
-    surprise = row[9]
-    score_val = row[10]
-    score_std = row[11]
-    score_min = row[12]
-    score_max = row[13]
-    score_avg = row[14]
+    smile = row[4]
+    #gender = row[1]
+    anger = row[5]
+    comtempt = row[6]
+    disgust = row[7]
+    fear = row[8]
+    happiness = row[9]
+    neutral = row[10]
+    sadness = row[11]
+    surprise = row[12]
+    score_val = row[13]
+    score_std = row[14]
+    score_min = row[15]
+    score_max = row[16]
+    score_avg = row[17]
     data =  {
 
             "Inputs": {
@@ -25,7 +25,7 @@ def send_request_to_ml(row):
                     {
                         "ColumnNames": ["smile", "gender", "anger", "contempt", "disgust", "fear", "happiness", "neutral", "sadness", "surprise", "score_val", "score_std", "score_min", "score_max", "score_avg"],
                         #"Values": [ [ "0", "male", "0", "0", "0", "0", "1", "0", "0", "0", "0", "0", "0", "0", "0" ] ]
-                        "Values": [ [ smile, gender, anger, comtempt, disgust, fear, happiness, neutral, sadness, surprise, score_val, score_std, score_min, score_max, score_avg ] ]
+                        "Values": [ [ str(smile), "male", str(anger), str(comtempt), str(disgust), str(fear), str(happiness), str(neutral), str(sadness), str(surprise), str(score_val), str(score_std), str(score_min), str(score_max), str(score_avg) ] ]
                     },        },
                     "GlobalParameters": {
                     }
