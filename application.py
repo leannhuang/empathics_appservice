89f48f814bd6dec8512511ee0e77680a69af27c5
LeAnn <aya65535@gmail.com>
2019-11-20 02:46:46 +0000

    m

--- application.py
+++ application.py
@@ -76,12 +76,12 @@ def handle_audio():
 
 @app.route('/post_pic', methods = ['GET','POST'])
 def handle_request():
-    # section_id = request.args.get('section_id')
-    # seq = request.args.get('seq')
-    # device_id = request.args.get('device_id')
-    session_id = 'aa80d283431542f5a16adcdac91365ee'
-    seq = 1
-    device_id = 'vuzix_us_1116'
+    section_id = request.args.get('section_id')
+    seq = request.args.get('seq')
+    device_id = request.args.get('device_id')
+    # session_id = 'aa80d283431542f5a16adcdac91365ee'
+    # seq = 1
+    # device_id = 'vuzix_us_1116'
     imagefile = request.files['image']
     filename = werkzeug.utils.secure_filename(imagefile.filename)
     imagefile.save(filename)
